4816c30f666a89d078a8cf8aea5ec68279e33d75
Maksym Krasnov <maksimkrasnov747@gmail.com>
2025-05-28 19:42:00 +0200

    feat: implement Tailwind as a framework and create logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,20 @@
 const scrollTo = (id: string) => {
-	document.getElementById(id)?.scrollIntoView({behavior: 'smooth'})
-}
+  document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
+};
 
 export default function Header() {
   return (
-    <header>
-      <button onClick={() => scrollTo("about")}>About</button>
-      <button onClick={() => scrollTo("route")}>Route</button>
-      <button onClick={() => scrollTo("gallery")}>Gallery</button>
-      <button onClick={() => scrollTo("support")}>Support</button>
-      <button onClick={() => scrollTo("contact")}>Contact</button>
+    <header className="bg-white text-white p-4 flex justify-between items-center fixed w-full z-50 shadow">
+      <div>
+        <img src="../../assets/logo.png" alt="" className='w-35'/>
+      </div>
+      <div>
+        <button onClick={() => scrollTo("about")}>About</button>
+        <button onClick={() => scrollTo("route")}>Route</button>
+        <button onClick={() => scrollTo("gallery")}>Gallery</button>
+        <button onClick={() => scrollTo("support")}>Support</button>
+        <button onClick={() => scrollTo("contact")}>Contact</button>
+      </div>
     </header>
   );
 }
